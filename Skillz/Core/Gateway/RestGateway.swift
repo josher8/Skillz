e0b6a987275f52ff5218d92639065f100c58a92b
Josh Slebodnik <slebodnik88@gmail.com>
2021-04-06 21:20:33 -0500

    Added RestGateway

---
+++ Skillz/Core/Gateway/RestGateway.swift
@@ -0,0 +1,27 @@
+import Alamofire
+
+typealias RestCompletion = (_ response: Data?, _ error: Error?) -> Void
+
+public enum RestMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+protocol RestProvider {
+    func request(url: String, method: RestMethod, parameters: [String: Any], headers: [String: String], completion: @escaping RestCompletion)
+}
+
+class RestGateway: RestProvider {
+    
+    init() {}
+    
+    func request(url: String, method: RestMethod, parameters: [String: Any], headers: [String: String], completion: @escaping RestCompletion) {
+        AF.request(url,
+                   method: HTTPMethod.init(rawValue: method.rawValue),
+                   parameters: parameters,
+                   encoding: JSONEncoding.default,
+                   headers: HTTPHeaders.init(headers)).response { response in
+                    completion(response.data, response.error)
+                   }
+    }
+}
